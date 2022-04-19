900570c2cc5e32e5aab82b95fe4419013ca855da
Tom Erickson <ericksont91@gmail.com>
2022-04-18 21:00:43 -0400

    changes

--- dist/main.js
+++ dist/main.js
@@ -1,109 +1,83 @@
-import "./style.css";
 import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+const canvas = document.querySelector("canvas.webgl");
 
+// Scene
 const scene = new THREE.Scene();
 
+const textureLoader = new THREE.TextureLoader();
+const myTexture = textureLoader.load("coolTex.jpg");
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry2 = new THREE.DodecahedronGeometry(0.5, 3);
+const material = new THREE.MeshBasicMaterial({
+  map: myTexture,
+});
+const boxMesh = new THREE.Mesh(geometry, material);
+const sphereMesh = new THREE.Mesh(geometry2, material);
+scene.add(boxMesh);
+// scene.add(sphereMesh)
+boxMesh.position.x = 0;
+boxMesh.position.y = 0.8;
+sphereMesh.position.x = -1.6;
+sphereMesh.position.y = 0.5;
+geometry.center();
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Renderer gets updated each time window is resized
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  sizes.width / sizes.height,
   0.1,
   100
 );
+camera.position.z = 3;
+scene.add(camera);
 
-const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector("#bg"),
+// Controls
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableZoom = false;
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
 });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+const clock = new THREE.Clock();
 
-renderer.render(scene, camera);
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  boxMesh.rotateX(30 * 0.0003);
+  boxMesh.rotateY(30 * 0.0003);
+  sphereMesh.rotateY(30 * 0.0003);
+  // mesh.position.y = Math.sin(elapsedTime) *0.1
+  boxMesh.position.z = Math.sin(elapsedTime) * 1;
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
-scene.add(pointLight);
-
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xfffff });
-  const star = new THREE.Mesh(geometry, material);
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-Array(200).fill().forEach(addStar);
-
-const spaceTexture = new THREE.TextureLoader().load("./assets/space.jpg");
-scene.background = spaceTexture;
-
-const jeffTexture = new THREE.TextureLoader().load("./assets/My project.png");
-
-const jeff = new THREE.Mesh(
-  new THREE.BoxGeometry(3, 3, 3),
-  new THREE.MeshBasicMaterial({ map: jeffTexture })
-);
-
-scene.add(jeff);
-const moonTexture = new THREE.TextureLoader().load("./assets/moon.jpg");
-const normalTexture = new THREE.TextureLoader().load("./assets/normal.jpg");
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
-);
-
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.setX(-10);
-
-jeff.position.z = -5;
-jeff.position.x = 2;
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
-
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
-}
-
-document.body.onscroll = moveCamera;
-moveCamera();
-
-function animate() {
-  requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-  controls.update;
+  controls.update();
+  controls.enableDamping = true;
   renderer.render(scene, camera);
-}
-animate();
+  window.requestAnimationFrame(tick);
+};
+
+tick();
